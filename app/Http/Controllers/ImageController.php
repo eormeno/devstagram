a58e1a9d7f0732ef8f8c8f2fb5502114b5775234
DAMO\Emilio <eormeno@gmail.com>
2023-06-05 11:00:46 -0300

    imagecontroller capture exception

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,16 +8,20 @@ use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
-    public function store(Request $request) {
-        $image = $request->file('file');
+    public function store(Request $request)
+    {
+        try {
+            $image = $request->file('file');
 
-        $imageName = Str::uuid() . '.' . $image->extension();
+            $imageName = Str::uuid() . '.' . $image->extension();
 
-        $imageToStore = Image::make($image);
-        $imageToStore->fit(1000, 1000);
-        $imageToStore->save(public_path('uploads/') . $imageName);
+            $imageToStore = Image::make($image);
+            $imageToStore->fit(1000, 1000);
+            $imageToStore->save(public_path('uploads/') . $imageName);
 
-        return response()->json(['image' => $imageName]);
+            return response()->json(['image' => $imageName]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
     }
 }
-
